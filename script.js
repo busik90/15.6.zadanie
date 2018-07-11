8541eed117d04040d4dab0f5d8b30b26a493dbd1
Pawel Bus <pawelbus.dev@outlook.com>
2018-07-11 12:14:22 +0200

    Add method format

--- script.js
+++ script.js
@@ -17,6 +17,18 @@ class Stopwatch {
   print() {
     this.display.innerText = this.format(this.times);
   }
+
+  format(times) {
+    function pad0(value) {
+      let result = value.toString();
+      if (result.length < 2) {
+          result = '0' + result;
+      }
+      return result;
+    }
+    
+    return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+  }
 }
 
 let startButton = document.getElementById('start');
