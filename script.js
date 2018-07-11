9c88110ff4e7ba449b00f544bf19571a7a630630
Pawel Bus <pawelbus.dev@outlook.com>
2018-07-11 16:07:41 +0200

    Change listeners variables to const

--- script.js
+++ script.js
@@ -61,10 +61,10 @@ class Stopwatch {
   }
 }
 
-let startButton = document.getElementById('start');
+const startButton = document.getElementById('start');
 startButton.addEventListener('click', () => stopwatch.start());
 
-let stopButton = document.getElementById('stop');
+const stopButton = document.getElementById('stop');
 stopButton.addEventListener('click', () => stopwatch.stop());
 
 const stopwatch = new Stopwatch(document.querySelector('.stopwatch'));
